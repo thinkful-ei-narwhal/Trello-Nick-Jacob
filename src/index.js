e5ae528a7b298b25afe4f44404355e3ae23af0b1
Jacob Flaxman <jacobflaxman1@gmail.com>
2020-04-06 09:06:07 -0800

    added css

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import STORE from './store';
+import './appStyles.css'
+import './cardStyles.css'
+import './listStyles.css'
 
 ReactDOM.render(<App store={STORE} />, document.getElementById('root'));
